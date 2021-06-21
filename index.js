5b640aee1875f9df0db8d5e9aed0788cda3aee50
Seo WooYoung <wyseo@seegene.com>
2021-06-21 17:10:44 +0900

    Add: 사용자 등록 기능 추가

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.get('/', (req, res) => { res.send('Hello World!') })
 
 app.post('/register', (req, res) => {
     const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({ success: true })
+    })
 })
 
 app.listen(port, () => {
