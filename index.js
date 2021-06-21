e0d99b4bb479a341edd9eab4cb877d3be86711de
Seo WooYoung <wyseo@seegene.com>
2021-06-21 16:36:14 +0900

    Fix: db 설정정보 수정

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var db_config  = require('./config/db.json');
+const bodyParser = require("body-parser")
+const { User } = require("./models/User")
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://wooyoung:1111@boilerplate.ay4o4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${db_config.user}:${db_config.password}@${db_config.host}/${db_config.database}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -10,8 +14,10 @@ mongoose.connect('mongodb+srv://wooyoung:1111@boilerplate.ay4o4.mongodb.net/myFi
 }).then(() => console.log('MongoDB Connected..'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/', (req, res) => { res.send('Hello World!') })
+
+app.post('/register', (req, res) => {
+    const user = User
 })
 
 app.listen(port, () => {
